ef30828e478cef3578a9a2c1600eb5f848334b31
Francisco Suárez <fsuarezm@gmail.com>
2023-07-24 13:20:04 +0200

    Commit inicial

---
+++ src/IdpersonaBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of fsuarezm/idpersona
+ *
+ * (c) Francisco Suárez Mulero
+ * @author: Francisco Suárez Mulero
+ * @email: fsuarezm@gmail.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace FSM\Symfony\Idpersona;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @final
+ */
+class IdpersonaBundle extends Bundle
+{
+}
